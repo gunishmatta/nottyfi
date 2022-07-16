7a5d8160e4175b1ff3d03fcfbc9c7663b0a375d3
gunishmatta <gunishmatta@gmail.com>
2022-07-16 17:58:17 +0530

    project init

---
+++ nottyfi-kafka-consumer/src/main/java/com/gunish/nottyfi/NottyfiKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.gunish.nottyfi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NottyfiKafkaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NottyfiKafkaProducerApplication.class, args);
+	}
+
+}
---
+++ nottyfi-kafka-producer/src/test/java/com/gunish/nottyfi/NottyfiKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gunish.nottyfi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NottyfiKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
